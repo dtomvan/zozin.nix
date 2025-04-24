39bf6d0b41ef530f021007c5597f9b9bd9da8f56
Tom van Dijk <18gatenmaker6@gmail.com>
2025-04-24 15:22:26 +0200

    blang: 0-unstable-2025-04-23 -> 0-unstable-2025-04-24

--- packages/blang/package.nix
+++ packages/blang/package.nix
@@ -6,35 +6,40 @@
   rustc,
   fasm,
   nob_h,
+  nix-update-script,
 }:
 clangStdenv.mkDerivation { # TODO: when Tsoding starts building with nob, use buildNobPackage
   pname = "b";
-  version = "0-unstable-2025-04-23";
+  version = "0-unstable-2025-04-24";
 
   src = fetchFromGitHub {
     owner = "tsoding";
     repo = "b";
-    rev = "66c4c1af2db8143d10596de675edae09bba9e37c";
-    hash = "sha256-DlPc/PDNQybefs+JQKBTIp45Tc3Bjg1DOeHNgPwe+S8=";
+    rev = "513cd864a2546c6bf61f2681d8e928fa1d2a26d7";
+    hash = "sha256-OaxB33466bs3dzccPCizgBZbFtdCXIjucEuEWvxjkto=";
   };
 
   patches = [(replaceVars ./use-nix-nob.patch {
     NOB_H = "${nob_h}/include/nob.h";
   })];
 
-  postPatch = "rm nob.h";
+  postPatch = "rm thirdparty/nob.h";
 
   nativeBuildInputs = [ rustc fasm ];
+
+  makeFlags = [ "build/b" "examples" ];
 
   installPhase = ''
   runHook preInstall
   
-  mkdir -p $out/bin $out/examples
-  cp b $out/bin
-  cp hello.js hello index.html $out/examples
+  mkdir -p $out/bin $out/opt/b/examples
+  cp build/b $out/bin
+  cp build/{hello.js,hello} index.html $out/opt/b/examples
 
   runHook postInstall
   '';
+
+  passthru.updateScript = nix-update-script { extraArgs = [ "--version=branch" ]; };
 
   meta = {
     description = "Compiler for the B Programming Language implemented in Crust";

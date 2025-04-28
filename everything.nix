f9b48b943401f2de658fce2990a6cc47980bb621
Tom van Dijk <18gatenmaker6@gmail.com>
2025-04-28 18:11:19 +0200

    move shell.nix -> everything.nix

---
+++ everything.nix
@@ -0,0 +1,9 @@
+{
+  pkgs ? import <nixpkgs> { },
+  ...
+}:
+pkgs.mkShell {
+  packages = builtins.filter (
+    p: builtins.isAttrs p && builtins.hasAttr "type" p && p.type == "derivation"
+  ) (builtins.attrValues (import ./default.nix { inherit pkgs; }));
+}
--- shell.nix
+++ shell.nix
@@ -1,9 +1,6 @@
-{
-  pkgs ? import <nixpkgs> { },
-  ...
-}:
-pkgs.mkShell {
-  packages = builtins.filter (
-    p: builtins.isAttrs p && builtins.hasAttr "type" p && p.type == "derivation"
-  ) (builtins.attrValues (import ./default.nix { inherit pkgs; }));
+{ pkgs ? import <nixpkgs> {}, ... }: pkgs.mkShell {
+  packages = with pkgs; [
+    nixfmt-rfc-style
+    treefmt
+  ];
 }

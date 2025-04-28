3340c7bbf4aa06d7474889a762eda9d58b5c8fd6
Tom van Dijk <18gatenmaker6@gmail.com>
2025-04-28 19:20:27 +0200

    [skip ci] fix: replace any formatter in legacy treefmt.toml

--- scripts/sync-treefmt-toml.sh
+++ scripts/sync-treefmt-toml.sh
@@ -5,4 +5,4 @@ echo "Generating legacy treefmt.toml..."
 config=`nix --extra-experimental-features "nix-command flakes" build .#treefmt-config --no-link --print-out-paths`
 install -m644 "$config" treefmt.toml
 
-sed -Ei 's|^command = "/nix/store/.*/bin/nixfmt"$|command = "nixfmt"|' treefmt.toml 
+sed -Ei 's|^command = "/nix/store/.*/bin/(.*)"$|command = "\1"|' treefmt.toml 
